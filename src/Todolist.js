8136e8ba8285c84b2492df689295c85f9c2b68cc
ChaoYIN <yinchao428@hotmail.com>
2019-06-21 12:13:08 +1000

    antd list

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,9 +1,34 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+import 'antd/dist/antd.css';
+import { Input, Button, List } from 'antd';
+
+const data = [
+  'Racing car sprays burning fuel into crowd.',
+  'Japanese princess to wed commoner.',
+  'Australian walks 100km after outback crash.',
+  'Man charged over missing wedding girl.',
+  'Los Angeles battles huge wildfires.',
+];
 
 class Todolist extends Component {
   render() {
     return (
-      <div>hello world </div>
+      <div style={{ marginLeft: "10px", marginTop: "10px" }}>
+        <div>
+          <Input placeholder='todo info' style={{ width: '300px' }} />
+          <Button type="primary" style={{ marginLeft: '10px' }}>Submit</Button>
+        </div>
+        <List
+          bordered
+          dataSource={data}
+          renderItem={item => (
+            <List.Item>
+              <Typography.Text mark>[ITEM]</Typography.Text> {item}
+            </List.Item>
+          )}
+        />
+      </div>
+
     )
   }
 }
