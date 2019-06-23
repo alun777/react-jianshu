7c2445d395e0ded0df445548ea702d7e715aff48
ChaoYIN <yinchao428@hotmail.com>
2019-06-23 22:58:52 +1000

    redux-middleware start

--- src/Todolist.js
+++ src/Todolist.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
 import 'antd/dist/antd.css';
-import { Input, Button, List } from 'antd';
 import store from "./store/index"
-
+import { getInputChangeAction, getAddItemAction, getDeleteItemAction, initListAction } from './store/actionCreators';
+import TodolistUI from './TodolistUI';
+import axios from 'axios';
 
 class Todolist extends Component {
 
@@ -12,42 +13,27 @@ class Todolist extends Component {
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleStoreChange = this.handleStoreChange.bind(this);
     this.handleBtnClick = this.handleBtnClick.bind(this);
+    this.handleItemDelete = this.handleItemDelete.bind(this);
     store.subscribe(this.handleStoreChange);
   }
 
   render() {
     return (
-      <div style={{ marginLeft: "10px", marginTop: "10px" }}>
-        <div>
-          <Input
-            placeholder='todo info'
-            style={{ width: '300px' }}
-            value={this.state.inputValue}
-            onChange={this.handleInputChange}
-          />
-          <Button 
-            type="primary" 
-            style={{ marginLeft: '10px' }}
-            onClick = {this.handleBtnClick}
-          >
-            Submit
-          </Button>
-        </div>
-        <List
-          style={{ width: '300px', marginTop: '10px' }}
-          bordered
-          dataSource={this.state.list}
-          renderItem={(item, index) => (
-            <List.Item 
-              onClick = {this.handleItemDelete.bind(this, index)}
-            >
-              {item}
-            </List.Item>
-          )}
-        />
-      </div>
+      <TodolistUI
+        inputValue={this.state.inputValue}
+        handleInputChange = {this.handleInputChange}
+        handleBtnClick = {this.handleBtnClick}
+        handleItemDelete = {this.handleItemDelete}
+        list={this.state.list}
+      />)
+  }
 
-    )
+  componentDidMount() {
+    axios.get('https://www.easy-mock.com/mock/5d0f1ffe7e96485dab376807/list.json').then((res)=>{
+      const data = res.data;
+      const action = initListAction(data);
+      store.dispatch(action);
+    })
   }
 
   handleStoreChange() {
@@ -55,25 +41,17 @@ class Todolist extends Component {
   }
 
   handleInputChange(e) {
-    const action = {
-      type: 'change_input_value',
-      value: e.target.value
-    }
+    const action = getInputChangeAction(e.target.value);
     store.dispatch(action);
   }
 
   handleBtnClick() {
-    const action = {
-      type: 'add_todo_item'
-    }
+    const action = getAddItemAction();
     store.dispatch(action);
   }
 
   handleItemDelete(index) {
-    const action = {
-      type: 'delete_todo_item',
-      index
-    }
+    const action = getDeleteItemAction(index);
     store.dispatch(action);
   }
 
---
+++ src/store/actionCreators.js
@@ -0,0 +1,20 @@
+export const getInputChangeAction = (value) => ({
+  type: 'change_input_value',
+  value
+});
+
+export const getAddItemAction = () => ({
+  type: 'add_todo_item'
+});
+
+export const getDeleteItemAction = (index) => ({
+  type: 'delete_todo_item',
+  index
+});
+
+export const initListAction = (data) => ({
+  type: 'init_list_action',
+  data
+});
+
+
